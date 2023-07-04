be9f6a49b21a20854fe8291674a577aa198b5817
zhangxiaotaotao <18706722515@sina.cn>
2023-07-04 21:37:27 +0800

    Roll back to default start spring boot application.

--- src/main/java/com/zjt/SpringBootHelloWorldApplication.java
+++ src/main/java/com/zjt/SpringBootHelloWorldApplication.java
@@ -7,14 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootHelloWorldApplication {
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
-//	}
-
 	public static void main(String[] args) {
-		var springApplication = new SpringApplication(SpringBootHelloWorldApplication.class);
-		springApplication.addInitializers(new ThirdInitializer());
-		springApplication.run(args);
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
+
+//	public static void main(String[] args) {
+//		var springApplication = new SpringApplication(SpringBootHelloWorldApplication.class);
+//		springApplication.addInitializers(new ThirdInitializer());
+//		springApplication.run(args);
+//	}
 
 }
